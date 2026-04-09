6b2b53864ccf249888cbce0319622627ee1303cf
Rareș Mărincaș <rares.marincas@lucaciu.ro>
2026-04-09 09:30:28 +0300

    Add files via upload

---
+++ opti.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="icon" href="Fv.png" type="image/jpg">
+    <link rel="stylesheet" href="Fizivision.css">
+    <style>
+    
+    li a{
+        color:white;
+        display:inline-block;
+        width:8%;
+        text-align: center;
+        
+    }
+    li {
+        display:inline;
+    }
+    input{
+        display:inline;
+        margin:0 auto;
+        width:8%;
+    }
+    label{
+        font-size:20px;
+    }
+    button{
+        height:60px;
+        float:right;
+    }
+    </style>
+    </head>
+<body style="background-color:#29104a">
+<li><a href="index.html">Acasă</a></li>
+            <li><a href="Meca.html">Mecanică</a></li>
+            <li><a href="Termodina.html">Termodinamică</a></li>
+            <li><a href="Opti.html">Optică</a></li>
+            <li><a href="Electri.html">Electricitate</a></li>
+            <li><a href="LaboDigi.html">Laborator Digital</a></li>
+            <li><a href="Contabili.html">Contact</a></li>
+    <body>
+        <div style="display: block; margin-top:30vh;background-image:url('fizi2.jpg');background-size:cover;height:300px">
+        <div style="background-color:black;">
+        <h2>Distanța focală</h2></div>
+        <div style="text-align:center">
+        <label for="raporti">Distanță obiect</label>
+        <button><a href="electri.php" style="float:right;font-size:69px;">></a></button>
+        <button><a href="termo.php" style="float:right;font-size:69px;"><</a></button>
+                <input type="number" placeholder="30" name="raporti" id="raporti">
+        <label for="coefi">Distanță imagine</label>
+        <input type="number" placeholder="60" name="coefi" id="coefi">
+                <br>
+        <button type="submit" onclick="schimba()" style="margin-right:500px;background-color:black;border-radius:20px;">Schimbă</button>
+        <br>
+    
+        <p style="background-color:black;">Distanța focală este <span id="rezultat">20</span></p>
+        </div>
+</div>
+</div>
+<script>
+function schimba()
+{
+    let n = parseFloat(document.getElementById('raporti').value);
+    let m = parseFloat(document.getElementById('coefi').value);
+    let randa = n*m/(n+m);
+    document.getElementById('rezultat').textContent = randa;
+}
+</script>
+    </body>
+</html>
